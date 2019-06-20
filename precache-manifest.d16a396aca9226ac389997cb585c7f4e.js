7fb6b1673acaa182409a7ac17219619c11f248fe
Gustavo Rueda <gustavorueda@MacBook-Pro-de-Gustavo.local>
2019-06-19 20:54:57 -0500

    Updates

---
+++ precache-manifest.d16a396aca9226ac389997cb585c7f4e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "41bf3241965cc7c2990174ed9d6e9714",
+    "url": "/texsul-manager/static/media/3.41bf3241.jpeg"
+  },
+  {
+    "revision": "3f8acb33d997ecfbbdbc",
+    "url": "/texsul-manager/static/css/main.edbe0c0d.chunk.css"
+  },
+  {
+    "revision": "fe349e8528c79623ea6795ddd110985a",
+    "url": "/texsul-manager/static/media/10.fe349e85.jpeg"
+  },
+  {
+    "revision": "1f34af59787b59a0fa1f",
+    "url": "/texsul-manager/static/js/1.1f34af59.chunk.js"
+  },
+  {
+    "revision": "c5e61b0ddb8decc297ee",
+    "url": "/texsul-manager/static/js/runtime~main.c5e61b0d.js"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "/texsul-manager/static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "60a859917fe65ea1f9f070e0dc080c76",
+    "url": "/texsul-manager/static/media/shards-dashboards-logo.60a85991.svg"
+  },
+  {
+    "revision": "ed986061ad9bf89c081724362956b134",
+    "url": "/texsul-manager/static/media/1.ed986061.jpeg"
+  },
+  {
+    "revision": "780ccb41c83dad3cbd24a9bcd3262586",
+    "url": "/texsul-manager/static/media/2.780ccb41.jpeg"
+  },
+  {
+    "revision": "3f8acb33d997ecfbbdbc",
+    "url": "/texsul-manager/static/js/main.3f8acb33.chunk.js"
+  },
+  {
+    "revision": "026cb7af3133d721efff2a0134f7e16f",
+    "url": "/texsul-manager/static/media/4.026cb7af.jpeg"
+  },
+  {
+    "revision": "6e846951d7a04c931a0f9efa94a1316f",
+    "url": "/texsul-manager/static/media/5.6e846951.jpeg"
+  },
+  {
+    "revision": "47789d678cfff97c48fda4de3f3621d5",
+    "url": "/texsul-manager/static/media/6.47789d67.jpeg"
+  },
+  {
+    "revision": "2835396c930c8bf53ffd3beeedba7c41",
+    "url": "/texsul-manager/static/media/7.2835396c.jpeg"
+  },
+  {
+    "revision": "10b775cc930b6986ec69eb9a4adef56a",
+    "url": "/texsul-manager/static/media/8.10b775cc.jpeg"
+  },
+  {
+    "revision": "565ad4a387d2090e5d73f0c7b8de2793",
+    "url": "/texsul-manager/static/media/9.565ad4a3.jpeg"
+  },
+  {
+    "revision": "1f34af59787b59a0fa1f",
+    "url": "/texsul-manager/static/css/1.3f008d51.chunk.css"
+  },
+  {
+    "revision": "53616f372895130ccd4d30225b51dc79",
+    "url": "/texsul-manager/index.html"
+  }
+];
